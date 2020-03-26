696b334014441064735f9a07a865791ec6a39cec
Curtis Chadwell <laurencechadwell@gmail.com>
2020-03-26 02:55:10 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do |drawf,index|
+    puts "#{index+1} #{drawf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planteers)
+  planteers.collect do |planteer|
+    "#{planteer.capitalize()}!"
+  end
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planteers)
+  planteers.any? do |planteer|
+    planteer.length > 4
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |chz|
+    cheese_types.include?(chz)
+  end
 end
