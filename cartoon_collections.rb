1b27121b5b55f5259aaedb591716bd314c4c6d04
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-02 01:36:59 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,8 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any? { |word| word.length > 4 }
-
-  end
+    array.any? { |word| word.length > 4 }
+end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
