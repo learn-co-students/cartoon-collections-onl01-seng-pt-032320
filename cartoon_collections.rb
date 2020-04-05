a945c976e8dc0b8f5ec57c1907cd924cc46d8ce2
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-05 07:09:46 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index { | name,index | puts "#{index+1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  returnAray = []
+  array.each {|name| returnAray << "#{name.capitalize}!"}
+  return returnAray
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? {|name| name.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.each {|name| 
+      if cheese_types.include?(name)
+        return name
+        return true
+      end
+    }
+  else
+    return nil
+  end
 end
